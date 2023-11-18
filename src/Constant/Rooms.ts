620794fb8fb9a7b4e77a3f7250663f9fc0a29d9e
Kevin <arthurkevin1260@gmail.com>
2023-11-18 21:14:59 +0000

    added

--- src/Constant/Rooms.ts
+++ src/Constant/Rooms.ts
@@ -1,36 +1,66 @@
-import rooms from '../assets/roomss.jpg'
-import rooms2 from '../assets/NUMBER-ONE-OXFORD02196.jpeg'
-import rooms3 from '../assets/pexels-suhel-vba-3659683.jpg'
-import rooms4 from '../assets/pexels-naim-benjelloun-2029698.jpg'
+import rooms4 from '../assets/roomss.jpg'
+import rooms6 from '../assets/NUMBER-ONE-OXFORD02196.jpeg'
+import rooms8 from '../assets/pexels-suhel-vba-3659683.jpg'
+import rooms10 from '../assets/pexels-naim-benjelloun-2029698.jpg'
+import rooms1 from '../assets/rooms1.webp'
+import rooms2 from '../assets/rooms2.jpeg'
+import rooms3 from '../assets/rooms3.jpeg'
+import rooms5 from '../assets/roomss.jpg'
+
+
 
 interface Rooms{
     id:number,
     imgUrl:string,
-    title:string
+    title:string,
+    content?:string,
+    items?:Array<{
+        id: number;
+        imgUrl: string;
+      }>;
+
 }
 export const Rooms:Rooms[]=[
     {id:1,
-    imgUrl:rooms,
-    title:'lorem ipsumeheheh'
+    imgUrl:rooms4,
+    title:'lorem ipsumeheheh',
+    content:"Are you balling on a budget? You don't have to settle our standard room is a cozy choice",
+    items:[
+        {
+            id:1,
+            imgUrl:rooms1
+        },{
+            id:2,
+            imgUrl:rooms2
+        },{
+            id:3,
+            imgUrl:rooms3
+        },
+        {
+            id:4,
+            imgUrl:rooms5
+        }
+    
+    ]
     },
     {id:2,
-        imgUrl:rooms2,
+        imgUrl:rooms6,
         title:'lorem ipsumeheheh'
         },
         {id:3,
-            imgUrl:rooms3,
+            imgUrl:rooms8,
             title:'lorem ipsumeheheh'
             }
             ,{id:4,
-                imgUrl:rooms4,
+                imgUrl:rooms10,
                 title:'lorem ipsumeheheh'
                 },
                 {id:5,
-                    imgUrl:rooms2,
+                    imgUrl:rooms8,
                     title:'lorem ipsumeheheh'
                     },
                     {id:6,
-                        imgUrl:rooms,
+                        imgUrl:rooms6,
                         title:'lorem ipsumeheheh'
                         }
 ]
--- src/RoutesPages/Roompage.tsx
+++ src/RoutesPages/Roompage.tsx
@@ -1,16 +1,260 @@
 import { useParams } from "react-router-dom";
+import {useState} from 'react'
 // import { RoomCards } from "../Components/RoomCards";
+import bgOne from '../assets/bgOne.jpg'
+import { Rooms as RoomConst } from "../Constant/Rooms";
+// import React, { useState } from 'react';
+// import DatePicker from 'react-datepicker';
+// import 'react-datepicker/dist/react-datepicker.css';
+// interface arguState{
+//     name:string,
+//     email:string,
+//     phone:string,
+//     checkInDate:Date|null,
+//     checkOutDate:Date|null,
+//     roomType:string,
+//     numberOfAdults:string,
+//     numberOfChildren:string
+// }
+
 const Roompage = () => {
     const {id}= useParams();
-    console.log(id);
+    const numId:number | undefined= id? parseInt(id,10):undefined
 
+const[display,setDisplay]=useState<string>('')
+const[displayValue,setDisplayValue]=useState<boolean>(false)
+    // console.log(id);
+const handleDisplay=(imgUrl:string)=>{
+setDisplay(imgUrl)
+setDisplayValue(!false)
+}
+
+    // const [formData, setFormData] = useState<arguState>({
+    //   name: '',
+    //   email: '',
+    //   phone: '',
+    //   checkInDate: null,
+    //   checkOutDate: null,
+    //   roomType: 'standard', 
+    //   numberOfAdults: '1', 
+    //   numberOfChildren: '0',
+    // });
+  
+    // const handleChange = (e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLSelectElement>) => {
+    //   const { name, value } = e.target;
+    //   setFormData((prevData) => ({
+    //     ...prevData,
+    //     [name]: value,
+    //   }));
+    // };
+    
+    
+    // const handleDateChange = (date: Date | null, field: string) => {
+    //   setFormData((prevData) => ({
+    //     ...prevData,
+    //     [field]: date,
+    //   }));
+    // };
+    
+    // const handleSubmit = (e:React.FormEvent) => {
+    //   e.preventDefault();
+    //   // Add your logic to handle the form data (e.g., send it to a server)
+    //   console.log('Form submitted:', formData);
+    // };
+  
+  
 
     return (  
-        <div>
-            Welcome to Room page 
+ <div>
+    
+ <div className="relative">
+      <img src={bgOne} className='w-full h-[100px] md:h-[250px] object-cover'/>   
+     <span className="absolute bottom-2 md:bottom-10 left-1/2 transform  -translate-x-1/2">
+<h1 id="header" className="font-medium text-[1.5rem] md:text-[3rem] text-white border-b-2 ">Premier Hotel</h1>
+     </span>
+     <div className="w-full absolute bg-black h-[100px] md:h-[250px] bg-opacity-40 top-0">
+
+     </div>
+     </div>
+ {/* <img src={display}/> */}
+ <div className='flex flex-row'>
+        <div className="mt-10 px-10">
+            
+<div className="flex flex-col gap-4">
+            {RoomConst.map((content) => (
+            <div
+              key={content.id}
+              className={`${numId === content.id ?'block':'hidden'}   `}
+            >
+              <img
+                loading="lazy"
+                src={displayValue ? display : content.imgUrl}
+                className="md:w-[600px] sm:h-[350px] h-[300px] object-cover"
+                alt=""
+              />
+              {/* <span className="flex flex-row justify-between py-4 items-center p-2">
+                <h2> {content.title}</h2>
+                <button  
+                className="bg-black text-white p-2 text-[0.8rem]">
+Book now                
+</button>
+              </span>
+            */ }      <div className="flex flex-row gap-[1px] mt-4 mb-4"> 
+                {
+                    content.items?.map((contents)=>(
+                        <div key={contents.id}
+                         className={`${numId === content.id ? 'block':'hidden'}`}>
+<img onClick={()=>handleDisplay(contents.imgUrl)}
+ src={contents.imgUrl} className="w-[150px] md:h-[100px] h-[100px] cursor-pointer object-cover"/>
+      
+                       </div>
+                    )
+
+                    )
+                }
+                </div>
+
+                <div className="w-[50%] flex flex-col gap-2">
+            <h1 className="font-medium text-[20px]">{content.title}</h1>
+            <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, rerum? Culpa numquam autem, optio saepe unde enim harum quam non aliquid eveniet nesciunt dolor dolorum animi dolore architecto quibusdam veritatis.</h2>
+            </div>
+            </div>
+          ))}
             
         </div>
-    );
+        </div>
+        
+         {/* <form onSubmit={handleSubmit} className=" flex flex-col w-[60%] px-10 gap-2  mb-20">
+    <h1 className='text-center font-semibold text-xl'>  Make a Reservation</h1>
+      <div className="mb-4">
+      
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+          className="mt-1 p-2 border outline-none  w-full"
+          required
+      placeholder='Name'
+      />
+      </div>
+
+      <div className="mb-4">
+       
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          className="mt-1 p-2 border  w-full outline-none"
+          placeholder='Email'
+          required
+        />
+      </div>
+
+      <div className="mb-4">
+        
+        <input
+          type="tel"
+          id="phone"
+          name="phone"
+          value={formData.phone}
+          onChange={handleChange}
+          placeholder='Phone'
+          className="mt-1 p-2 border  w-full outline-none"
+          required
+        />
+      </div>
+
+<div className='flex flex-row gap-4'>
+      <div className="mb-4">
+        <DatePicker
+       placeholderText='Check-in Date'
+       selected={formData.checkInDate}
+          onChange={(date) => handleDateChange(date, 'checkInDate')}
+          className="mt-1 p-2 border  w-full"
+          dateFormat="MM/dd/yyyy"
+          minDate={new Date()}
+          required
+        />
+      </div>
+
+      <div className="mb-4">
+        <DatePicker
+       placeholderText='Check-out Date'
+       selected={formData.checkOutDate}
+          onChange={(date) => handleDateChange(date, 'checkOutDate')}
+          className="mt-1 p-2 border  w-full"
+          dateFormat="MM/dd/yyyy"
+          minDate={formData.checkInDate || new Date()}
+          required
+        />
+      </div>
+      </div>
+      <div className="mb-4">
+        
+        <select
+          id="roomType"
+          name="roomType"
+          value={formData.roomType}
+          onChange={handleChange}
+          placeholder='Room Type'
+          className="mt-1 p-2 border  w-full"
+        >
+          <option value="standard">Standard Room</option>
+          <option value="deluxe">Deluxe Room</option>
+          <option value="suite">Suite</option>
+        </select>
+      </div>
+
+<div className='flex flex-row gap-4'>
+      <div className="mb-4">
+        
+        <select
+          id="numberOfAdults"
+          name="numberOfAdults"
+          value={formData.numberOfAdults}
+          onChange={handleChange}
+        placeholder='Number of Adults'
+          className="mt-1 p-2 border  w-full"
+        >
+          {Array.from({ length: 5 }, (_, i) => i + 1).map((num) => (
+            <option key={num} value={String(num)}>
+              {num}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <div className="mb-4">
+    
+        <select
+          id="numberOfChildren"
+          name="numberOfChildren"
+          value={formData.numberOfChildren}
+          onChange={handleChange}
+          className="mt-1 p-2 border  w-full"
+    placeholder='Number of Children'
+    >
+          {Array.from({ length: 5 }, (_, i) => i).map((num) => (
+            <option key={num} value={String(num)}>
+              {num}
+            </option>
+          ))}
+        </select>
+      </div>
+      </div>
+      <div className="mt-4">
+        <button type="submit" className="bg-black text-white p-2 w-[100%]">
+          Book Now
+        </button>
+      </div>
+    </form>
+    */}
+        </div>
+        </div>);
 }
  
 export default Roompage;
